b86004dc5188794ee9d3c8efe7f2f761267a71ba
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-11-27 12:01:49 -0300

    mobile

--- src/components/Main/style.ts
+++ src/components/Main/style.ts
@@ -57,4 +57,46 @@ export const StyledMain = styled.main`
       color: white;
     }
   }
+
+  @media (max-width: 800px) {
+    .main {
+      &__title {
+        font-size: 4rem;
+
+        margin-bottom: 5rem;
+      }
+      &__form {
+        &__container {
+          gap: 1rem;
+        }
+        &__input {
+          width: 25rem;
+          height: 5rem;
+          padding-left: 0.3rem;
+          font-size: 2rem;
+          font-family: inherit;
+        }
+        &__input::placeholder {
+          color: black;
+          opacity: 0.2;
+        }
+        &__button {
+          font-family: inherit;
+          background-color: white;
+          color: black;
+          height: 5rem;
+          padding: 0.5rem 0.5rem;
+          font-size: 2rem;
+          border: 1px solid white;
+          cursor: pointer;
+          border-radius: 0.6rem;
+        }
+      }
+      &__link {
+        margin-top: 3rem;
+        font-size: 2rem;
+        color: white;
+      }
+    }
+  }
 `;
